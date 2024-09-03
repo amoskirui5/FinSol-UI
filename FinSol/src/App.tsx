af4c75f7e9701c0ceb7b93cb2cd583d3767528c5
amoskirui5 <amoskirui5@gmail.com>
2024-09-03 17:42:38 +0300

    -add menu to the side bar

--- FinSol/src/App.tsx
+++ FinSol/src/App.tsx
@@ -2,9 +2,26 @@ import React, { useState } from 'react';
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
-  UploadOutlined,
+  SettingOutlined,
   UserOutlined,
-  VideoCameraOutlined,
+  MoneyCollectOutlined,
+  UsergroupAddOutlined,
+  OrderedListOutlined,
+  UserAddOutlined,
+  CheckCircleOutlined,
+  WalletOutlined,
+  FileAddOutlined,
+  AuditOutlined,
+  StockOutlined,
+  ApartmentOutlined,
+  TeamOutlined,
+  IdcardOutlined,
+  FileSearchOutlined,
+  MailOutlined,
+  DatabaseOutlined,
+  SafetyOutlined,
+  LockOutlined,
+  MobileOutlined
 } from '@ant-design/icons';
 import { Button, Layout, Menu, theme } from 'antd';
 
@@ -18,29 +35,179 @@ const App: React.FC = () => {
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
+      <Sider 
+      trigger={null} 
+      collapsible 
+      collapsed={collapsed}
+      style={{
+        borderRight: '1px solid lightgray',
+        overflow: 'hidden', 
+        
+        position: 'sticky',
+        top: '0px',
+        backgroundColor: 'white',
+        minWidth: '550px', 
+      }}
+      width={collapsed ? 80 : 'auto'}
+
+      >
         <div className="demo-logo-vertical" />
         <Menu
-          theme="dark"
+          style={{
+            backgroundColor: 'white',
+            fontWeight: 700,
+            overflowY: 'auto',
+            overflowX: 'hidden',
+          }}
           mode="inline"
           defaultSelectedKeys={['1']}
           items={[
             {
               key: '1',
               icon: <UserOutlined />,
-              label: 'nav 1',
+              label: 'Members',
+              children: [
+                {
+                  key: '1-1',
+                  icon: <OrderedListOutlined />,
+                  label: 'Member List',
+                },
+                {
+                  key: '1-2',
+                  icon: <UsergroupAddOutlined />,
+                  label: 'Next Of Kin',
+                },
+                {
+                  key: '1-3',
+                  icon: <UserAddOutlined />,
+                  label: 'Member Registration',
+                },
+              ],
             },
             {
               key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
+              icon: <MoneyCollectOutlined />,
+              label: 'Loans',
+              children: [
+                {
+                  key: '2-1',
+                  icon: <FileAddOutlined />,
+                  label: 'Loan Applications',
+                },
+                {
+                  key: '2-2',
+                  icon: <CheckCircleOutlined />,
+                  label: 'Loan Approvals',
+                },
+                {
+                  key: '2-3',
+                  icon: <WalletOutlined />,
+                  label: 'Loan Disbursements',
+                }
+              ]
             },
             {
               key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
+              icon: <AuditOutlined />,
+              label: 'Member Funds',
+              children: [
+                {
+                  key: '3-1',
+                  icon: <WalletOutlined />,
+                  label: 'Member Deposits'
+                },
+                {
+                  key: '3-2',
+                  icon: <StockOutlined />,
+                  label: 'Member Share Capital'
+                }
+
+              ]
+            },
+
+            {
+              key: '4',
+              icon: <SettingOutlined />,
+              label: 'Settings',
+              children:
+                [
+                  {
+                    key: '4-1',
+                    icon: <ApartmentOutlined />,
+                    label: 'Administration'
+                  },
+                  {
+                    key: '4-2',
+                    icon: <TeamOutlined />,
+                    label: 'User Management',
+                    children: [
+                      {
+                        key: '4-2-1',
+                        icon: <IdcardOutlined />,
+                        label: 'User Roles',
+                      },
+                      {
+                        key: '4-2-2',
+                        icon: <UserOutlined />,
+                        label: 'User Accounts',
+                      },
+                      {
+                        key: '4-2-3',
+                        icon: <FileSearchOutlined />,
+                        label: 'Access Logs',
+                      },
+                    ],
+                  },
+                  {
+                    key: '4-3',
+                    icon: <SettingOutlined />,
+                    label: 'System Settings',
+                    children: [
+                      {
+                        key: '4-3-1',
+                        icon: <SettingOutlined />,
+                        label: 'General Settings',
+                      },
+                      {
+                        key: '4-3-2',
+                        icon: <MailOutlined />,
+                        label: 'Email Settings',
+                      },
+                      {
+                        key: '4-3-3',
+                        icon: <DatabaseOutlined />,
+                        label: 'Backup & Restore',
+                      },
+                    ],
+                  },
+                  {
+                    key: '4-4',
+                    icon: <SafetyOutlined />,
+                    label: 'Security',
+                    children: [
+                      {
+                        key: '4-4-1',
+                        icon: <LockOutlined />,
+                        label: 'Password Policy',
+                      },
+                      {
+                        key: '4-4-2',
+                        icon: <MobileOutlined />,
+                        label: 'Two-Factor Authentication',
+                      },
+                      {
+                        key: '4-4-3',
+                        icon: <AuditOutlined />,
+                        label: 'Audit Trail',
+                      },
+                    ],
+                  }
+
+                ]
             },
           ]}
+          
+          
         />
       </Sider>
       <Layout>
