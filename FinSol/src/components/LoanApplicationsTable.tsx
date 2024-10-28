ff15db4a83bd9f9f51991d8c28538e882b09f329
amoskirui5 <amoskirui5@gmail.com>
2024-10-28 23:18:42 +0300

    -add service for loan disbursements and validations

--- FinSol/src/components/LoanApplicationsTable.tsx
+++ FinSol/src/components/LoanApplicationsTable.tsx
@@ -5,7 +5,7 @@ import { fetchLoanApplications, fetchLoanEligibility } from '../services/memberL
 import { LoanApplicationList, LoanInfoResponseDTO } from '../types/MemberLoan/memberLoanTypes';
 import { PaginationOptions } from '../types/paginationTypes';
 import { formatCurrency } from '../Utility/formatCurrency';
-import { CheckCircleOutlined, CloseCircleOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import { CheckCircleOutlined, CloseCircleOutlined, DollarCircleOutlined, InfoCircleOutlined } from '@ant-design/icons';
 import LoanEligibilityModal from './LoanEligibilityModal';
 import LoanApprovalConfirmationModal from './LoanApprovalConfirmationModal';
 import { useNavigate } from 'react-router-dom';
@@ -50,7 +50,7 @@ const LoanApplicationsTable: React.FC = () => {
     } else {
       console.log('Could not fetch eligibility info:', eligibilityResponse.message);
     }
-    
+
   };
 
   const handleConfirmApproval = (loanId: string) => {
@@ -58,7 +58,6 @@ const LoanApplicationsTable: React.FC = () => {
     setIsConfirmModalVisible(false);
   };
 
-  // Handle cancel of confirmation
   const handleCancelApproval = () => {
     setIsConfirmModalVisible(false);
   };
@@ -84,7 +83,12 @@ const LoanApplicationsTable: React.FC = () => {
 
   const handleContinueApproval = () => {
     navigate(`/loan-approval/${memLoanId}`, { state: { loanInfo } });
-  }
+  };
+
+  const handleDisburseLoan = async (loanAppId: string) => {
+    
+    navigate(`/loan-disbursement/${loanAppId}`);
+  };
 
   const columns = [
     {
@@ -135,24 +139,38 @@ const LoanApplicationsTable: React.FC = () => {
       key: 'actions',
       render: (text: any, record: any) => (
         <div>
-          <Tooltip title="Approve Loan">
+          <Tooltip title={record.loanStatus === LoanStatus.Approved ? "Loan Approved" : "Approve Loan"}>
             <Button
               type="primary"
               icon={<CheckCircleOutlined />}
               onClick={() => handleApproveLoan(record.loanId, record.memberId, record.loanTypeId, record.amount)}
-              disabled={record.loanStatus==LoanStatus.Approved}
+              disabled={record.loanStatus !== LoanStatus.Pending ||record.loanStatus !== LoanStatus.Applied}
               style={{ marginRight: 8 }}
             />
           </Tooltip>
-          <Tooltip title="Decline Loan">
+
+          <Tooltip title={record.loanStatus === LoanStatus.Approved ? "Loan Approved" : "Decline Loan"}>
             <Button
               danger
               icon={<CloseCircleOutlined />}
               onClick={() => handleDeclineLoan(record.loanId)}
-              disabled={record.loanStatus==LoanStatus.Approved}
+              disabled={record.loanStatus !== LoanStatus.Pending ||record.loanStatus !== LoanStatus.Applied}
               style={{ marginRight: 8 }}
             />
           </Tooltip>
+
+          {record.loanStatus === LoanStatus.Approved && (
+            <Tooltip title={record.loanStatus === LoanStatus.Disbursed ? "Loan already disbursed" : "Disburse Loan"}>
+              <Button
+                type="primary"
+                icon={<DollarCircleOutlined />}
+                onClick={() => handleDisburseLoan(record.loanId)}
+                disabled={record.loanStatus === LoanStatus.Disbursed}
+                style={{ marginRight: 8 }}
+              />
+            </Tooltip>
+          )}
+
           <Tooltip title="Check Member Info">
             <Button
               type="default"
@@ -162,6 +180,7 @@ const LoanApplicationsTable: React.FC = () => {
           </Tooltip>
         </div>
       ),
+
     },
   ];
 
---
+++ FinSol/src/views/MemberAccount/LoanDisbursementForm.tsx
@@ -0,0 +1,155 @@
+import React, { useEffect, useState } from 'react';
+import { Form, Input, Button, Select, DatePicker, message } from 'antd';
+import { useParams } from 'react-router-dom';
+import { LoanDisbursementRequestDTO } from '../../types/loanTypeTypes';
+import { fetchLoanApprovalDetailsById, submitLoanDisbursement } from '../../services/memberLoanService';
+import { getChartOfAccounts } from '../../services/chartOfAccountsService';
+import { ChartOfAccount } from '../../types/accountingTypes';
+import { alertService } from '../../services/alertService';
+import { LoanApprovalListDTO } from '../../types/MemberLoan/memberLoanTypes';
+import { formatDate } from '../../helpers/dateFormater';
+
+const { Option } = Select;
+
+const LoanDisbursementForm: React.FC = () => {
+    const { loanApplicationId } = useParams<{ loanApplicationId: string }>();
+    const [form] = Form.useForm();
+    const [debitAccounts, setDebitAccounts] = useState<ChartOfAccount[]>([]);
+    const { showAlert } = alertService();
+    const [loanApproval, setLoanApproval] = useState<LoanApprovalListDTO | null>(null);
+
+    useEffect(() => {
+        const fetchDebitAccounts = async () => {
+            const response = await getChartOfAccounts();
+            if (response.success) {
+                setDebitAccounts(response.data);
+
+            }
+        };
+
+        fetchDebitAccounts();
+    }, []);
+
+    useEffect(() => {
+        const fetchApprovalData = async () => {
+            if (!loanApplicationId) {
+                message.error("Loan application ID is missing.");
+                return;
+            }
+            var approvalData = await fetchLoanApprovalDetailsById(loanApplicationId);
+            if (approvalData.success) {
+                setLoanApproval(approvalData.data);
+            }
+        }
+        fetchApprovalData();
+    }, [loanApplicationId]);
+
+    const handleSubmit = async (values: any) => {
+        const disbursementData: LoanDisbursementRequestDTO = {
+            ...values,
+            loanAppId: loanApplicationId,
+            memberId:loanApproval?.memberId,
+            dateDisbursed: values.dateDisbursed.format(),
+            loanTypeId:loanApproval?.loanTypeId
+        };
+
+        if (disbursementData.amount <= 0) {
+            return showAlert('Error', 'Disbursed amount cannot be zero or less', 'error');
+        }
+
+        if (loanApproval) {
+
+            if (disbursementData.amount > loanApproval?.amount) {
+                return showAlert('Error', `Disbursed amount ${disbursementData.amount} cannot be more than approved amount ${loanApproval?.amount}`, 'error');
+            }
+
+            if (disbursementData.dateDisbursed < loanApproval.approvalDate) {
+                return showAlert('Error', `Disbursement date ${formatDate(disbursementData.dateDisbursed)} cannot be beofre approval date ${formatDate(loanApproval?.approvalDate)}`, 'error');
+            }
+        }
+
+        await submitLoanDisbursement(disbursementData);
+
+        form.resetFields();
+
+    };
+
+    useEffect(() => {
+        if (loanApproval) {
+            form.setFieldsValue({ amount: loanApproval.amount });
+        }
+    }, [loanApproval, form]);
+
+    return (
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={handleSubmit}
+        >
+            <Form.Item
+                label="Date Disbursed"
+                name="dateDisbursed"
+                rules={[{ required: true, message: 'Please select the date disbursed' }]}
+            >
+                <DatePicker />
+            </Form.Item>
+
+            <Form.Item
+                label="Amount"
+                name="amount"
+                rules={[{ required: true, message: 'Please input the amount' }]}
+            >
+                <Input type="number" />
+            </Form.Item>
+
+            <Form.Item
+                label="Account Number"
+                name="accountNumber"
+                rules={[{ required: true, message: 'Please input the account number' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Transaction Reference"
+                name="transactionReference"
+                rules={[{ required: true, message: 'Please input the transaction Reference' }]}
+            >
+                <Input />
+            </Form.Item>
+            <Form.Item
+                label="Payment Method"
+                name="paymentMethod"
+                rules={[{ required: true, message: 'Please select a Payment method' }]}
+            >
+                <Select placeholder="Select a payment method" style={{ width: '100%' }}>
+                    <Option value="cash">Cash</Option>
+                    <Option value="bank_transfer">Bank Transfer</Option>
+                    <Option value="cheque">Cheque</Option>
+                </Select>
+            </Form.Item>
+
+            <Form.Item
+                label="Debit Account"
+                name="debitAccountId"
+                rules={[{ required: true, message: 'Please select a debit account' }]}
+            >
+                <Select placeholder="Select a debit account">
+                    {debitAccounts.map((account) => (
+                        <Option key={account.id} value={account.id}>
+                            {account.accountName}
+                        </Option>
+                    ))}
+                </Select>
+            </Form.Item>
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit">
+                    Submit Disbursement
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default LoanDisbursementForm;
