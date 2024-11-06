55266d912f4f000e3d982077741c262d7ce6fc44
amoskirui5 <amoskirui5@gmail.com>
2024-11-06 16:16:41 +0300

    --add balansheet page and endpoint call

--- FinSol/src/services/financialStatement.ts
+++ FinSol/src/services/financialStatement.ts
@@ -1,6 +1,6 @@
-import { FETCH_TRIAL_BALANCE } from "../constants/apiEndpoints";
+import { FETCH_BALANCE_SHEET, FETCH_TRIAL_BALANCE } from "../constants/apiEndpoints";
 import axiosInstance from "../interceptors/globaInterceptor";
-import { TrialBalanceResponse } from "../types/Financials/FinancialTypes";
+import { BalanceSheetResponse, TrialBalanceResponse } from "../types/Financials/FinancialTypes";
 
 export const fetchTrialBalance = async (startDate?: string, endDate?: string): Promise<TrialBalanceResponse> => {
     const response = await axiosInstance.get(FETCH_TRIAL_BALANCE,
@@ -9,5 +9,11 @@ export const fetchTrialBalance = async (startDate?: string, endDate?: string): P
                 startDate, endDate
             }
         });
+    return response.data;
+}
+
+export const fetchBalanceSheet = async (years: number[] | null): Promise<BalanceSheetResponse> => {
+    const payload = years || [];
+    const response = await axiosInstance.post(FETCH_BALANCE_SHEET, payload);
     return response.data;
 }
