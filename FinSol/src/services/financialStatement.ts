8905b1a93f336315021376122e01fdff5d648554
amoskirui5 <amoskirui5@gmail.com>
2024-11-11 23:17:22 +0300

    -add cashbook endpoints and display

--- FinSol/src/services/financialStatement.ts
+++ FinSol/src/services/financialStatement.ts
@@ -1,6 +1,6 @@
-import { FETCH_BALANCE_SHEET, FETCH_TRIAL_BALANCE } from "../constants/apiEndpoints";
+import { FETCH_BALANCE_SHEET, FETCH_CASH_BOOK, FETCH_TRIAL_BALANCE } from "../constants/apiEndpoints";
 import axiosInstance from "../interceptors/globaInterceptor";
-import { BalanceSheetResponse, TrialBalanceResponse } from "../types/Financials/FinancialTypes";
+import { BalanceSheetResponse, CashbookRecordResponse, TrialBalanceResponse } from "../types/Financials/FinancialTypes";
 
 export const fetchTrialBalance = async (startDate?: string, endDate?: string): Promise<TrialBalanceResponse> => {
     const response = await axiosInstance.get(FETCH_TRIAL_BALANCE,
@@ -15,5 +15,16 @@ export const fetchTrialBalance = async (startDate?: string, endDate?: string): P
 export const fetchBalanceSheet = async (years: number[] | null): Promise<BalanceSheetResponse> => {
     const payload = years || [];
     const response = await axiosInstance.post(FETCH_BALANCE_SHEET, payload);
+    return response.data;
+}
+
+
+export const fetchCashBook = async (accountId:string,startDate?: string | null, endDate?: string|null): Promise<CashbookRecordResponse> => {
+    const response = await axiosInstance.get(FETCH_CASH_BOOK+`/${accountId}`,
+        {
+            params: {
+                startDate, endDate
+            }
+        });
     return response.data;
 }
